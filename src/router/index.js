91665a63b7aeaae7b2dc62ea9ba8378d2cd76bd1
hana-zf <100548128+hana-zf@users.noreply.github.com>
2022-10-17 08:10:05 +0800

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -13,14 +13,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = new VueRouter({
